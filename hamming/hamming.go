1684228010a88950f3384a4b9d4f6325b4f146c3
ziauddinziya07 <54236981+ziauddinziya07@users.noreply.github.com>
2020-06-29 12:10:45 +0530

    Update hamming.go

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -4,14 +4,16 @@ import "errors"
 
 func Distance(a, b string) (int, error) {
 
-	if len(a) == len(b) {
-		c := 0
-		for i := 0; i < len(a); i ++ {
-			if a[i] != b[i] {
-				c ++
-			}
-		}
-		return c, nil
+	if len(a) != len(b) {
+		
+		return 0, errors.New("")
 	}
-	return 0, errors.New("")
+	c := 0
+	for i := 0; i < len(a); i ++ {
+		if a[i] != b[i] {
+			c ++
+		}
+	}
+	return c, nil
+
 }
