2577a1fa7e4a912057baff6bca8aff8fa52e53ef
ziauddinziya07 <54236981+ziauddinziya07@users.noreply.github.com>
2020-07-10 10:00:47 +0530

    Update hamming.go

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -2,11 +2,12 @@ package hamming
 
 import "errors"
 
+// Distance returns the differences between the two Strads and error as nil when stards are of Equal length ....
 func Distance(a, b string) (int, error) {
 
 	if len(a) != len(b) {
 		
-		return 0, errors.New("")
+		return 0, errors.New("Unequal Strards")
 	}
 	c := 0
 	for i := 0; i < len(a); i ++ {
@@ -15,5 +16,4 @@ func Distance(a, b string) (int, error) {
 		}
 	}
 	return c, nil
-
 }
